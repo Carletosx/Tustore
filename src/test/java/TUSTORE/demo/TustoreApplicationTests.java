bcfa705f2a73b94fe5e79a90cded04ae5fa639a0
Carletosx <carlosferreiraernesto@gmail.com>
2025-05-20 15:14:10 -0500

    Primer commit: Configuración inicial del proyecto Spring Boot

---
+++ src/test/java/TUSTORE/demo/TustoreApplicationTests.java
@@ -0,0 +1,13 @@
+package TUSTORE.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TustoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
